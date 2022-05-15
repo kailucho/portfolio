475fc107760e49c64415650c5800738c9d2cc99f
kaido <luijhy9234@gmail.com>
2022-05-15 02:18:16 -0500

    new item portfolio

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,17 +3,25 @@ import React from "react";
 import "./portfolio.css";
 import IMG1 from "../../assets/portfolio1.png";
 import IMG2 from "../../assets/portfolio2.png";
+import IMG3 from "../../assets/portfolio3.png";
 
 const data = [
   {
     id: 1,
+    image: IMG3,
+    title: "contenful nextjs recipes",
+    github: "https://github.com/kailucho/contenful-nextjs-recipes",
+    demo: "https://contenful-nextjs-recipes.vercel.app/",
+  },
+  {
+    id: 2,
     image: IMG1,
     title: "covid-19 tracker ",
     github: "https://github.com/kailucho/covid19-tracker",
     demo: "https://covid19-tracker-kaido.netlify.app/",
   },
   {
-    id: 2,
+    id: 3,
     image: IMG2,
     title: "Amazon clone ",
     github: "https://github.com/kailucho/amazon-clone",
